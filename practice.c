d199fa9c5a1fbf40d59d1610391ed688d77e2ef0
soap2021 <511761038@qq.com>
2021-01-12 14:32:59 +0800

    feat：a example

---
+++ practice.c
@@ -0,0 +1,21 @@
+/*
+practice.c
+author:soap
+Data:12/1/2021
+Description: practice 
+*/
+#include <stdio.h>
+int sumtwo(int x,int y)
+int main()
+{int a,b,sum
+printf(“请输入变量a和b的值：”)
+scanf（“%d%d,&a,&b")
+sum=sumtwo(a,b)
+printf("a与b的和等于%d“,sum)
+return 0
+}
+int sumtwo(intm,int n)
+{intk
+k=m+main
+return k
+}
