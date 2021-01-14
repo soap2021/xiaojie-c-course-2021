6b786d7c86c1af6c8dd0e5d38cc0ca8eec5d182a
soap2021 <511761038@qq.com>
2021-01-14 15:56:02 +0800

    1.4-feat: 完成1_4_1
    .c
    .exe

---
+++ 1.4/1_4_1.c
@@ -0,0 +1,16 @@
+/*
+ 1_4_1.c
+ Author:soap
+ Date:14/1/2021
+ Description:Output two lines
+*/
+
+
+#include <stdio.h>
+
+int main(){
+    
+    printf("We learn C language.\nWe use the Internet."); // '\n' creates a new line char. 
+    
+    return 0;
+}
