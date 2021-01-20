5b1e37b4b23547e077d0c820a66188deebf8789c
soap2021 <511761038@qq.com>
2021-01-20 23:32:43 +0800

    feat:HW

--- 4.2/hw_19_01_2019.c
+++ 4.2/hw_19_01_2019.c
@@ -7,4 +7,21 @@ Output: 2
 Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. 
              Since 2 has only one digit, return it.*/
 
-             
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+ 
+    printf("please input a number:  ");
+    scanf("%d",&a);
+
+
+    for(a;a<1;){
+        b=a%10;
+        c=c+b;
+        a=a/10;
+           
+    }  
+   printf("the output is %d",c);
+   return 0;
+}
